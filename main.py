5586b87fa36d3e012905de50ccdb3e3c09a6e67d
gabriele.stella <gabriele.stella@marconicloud.it>
2025-05-19 09:21:06 +0000

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="src")
+
+posts = [
+    {'title': 'Don Chisciotte della Mancia', 'author': 'Miguel de Cervantes'},
+    {'title': 'A Tale of Two Cities', 'author': 'Charles Dickens'},
+    {'title': 'Il Signore degli Anelli', 'author': 'J.R.R. Tolkien'},
+    {'title': 'Il Piccolo Principe', 'author': 'Antoine de Saint-Exupéry'},
+    {'title': 'Harry Potter e la Pietra Filosofale', 'author': 'J.K. Rowling'}
+]
 
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html', posts=posts)
+
+@app.route
+def serve_index():
+    return send_file('index.html')
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
